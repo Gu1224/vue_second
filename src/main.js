375cd7b40e3e94b1562211f25d09c1432540ee2f
Gu1224 <39993947+Gu1224@users.noreply.github.com>
2021-06-17 19:19:04 +0800

    Update main.js
    添加了element和global样式的调用

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import '../src/plugins/element'
+import '../src/assets/css/global.css'
+
 Vue.config.productionTip = false
 
 new Vue({
